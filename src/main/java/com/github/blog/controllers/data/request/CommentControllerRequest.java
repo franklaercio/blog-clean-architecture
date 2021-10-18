01fa6391a5b06d4351325e8e48834bb9854cdad7
Frank Laércio <franklaercio@gmail.com>
2021-10-18 17:37:42 -0300

    Ajuste de problema de bind

--- src/main/java/com/github/blog/controllers/data/request/CommentControllerRequest.java
+++ src/main/java/com/github/blog/controllers/data/request/CommentControllerRequest.java
@@ -1,10 +1,8 @@
 package com.github.blog.controllers.data.request;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class CommentControllerRequest {
 
     private String personName;
