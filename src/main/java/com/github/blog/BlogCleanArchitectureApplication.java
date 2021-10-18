769955d47289ae88c814342363797700be27bd1a
Frank Laércio <franklaercio@gmail.com>
2021-10-17 22:59:43 -0300

    Adding base project

---
+++ src/main/java/com/github/blog/BlogCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.github.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/blog/BlogCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
